4b46f600bb8f013acb76c06f77e2b370ca2d9c5c
Sam Farmer <sam@rocketscienceamusements.com>
2025-04-17 20:58:48 -0700

    Cut unused constants

--- src/Constants.cs
+++ src/Constants.cs
@@ -4,26 +4,11 @@ public static class Dimensions
 {
 	public const int GAME_W = 640;
 	public const int GAME_H = 360;
-	public const int CARD_WIDTH = 120;
-
 }
 
 
 public static class FontSizes
 {
 	public const int SCORE = 12;
-	public const int ORDER = 10;
-	public const int SMALL_ORDER = 8;
-	public const int DIALOGUE = 10;
-	public const int INSPECT = 10;
-	public const int INGREDIENT = 8;
-	public const int HOLDING = 10;
 	public const int SCORE_STRING = 20;
-}
-
-public static class Time
-{
-	public const float ROUND_TIME = 90.0f;
-	public const float CATEGORY_ORDER_TIME = 20.0f;
-	public const float INGREDIENT_ORDER_TIME = 30.0f;
 }
